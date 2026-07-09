33c829c6503f14cb57b129ae73bc87db4286234c
RodgersBrian <108925981+rodgersBRY@users.noreply.github.com>
2026-07-09 08:34:50 +0300

    FIX: update widget test assertion for the Baberia Cuts rename

--- mobile/test/widget_test.dart
+++ mobile/test/widget_test.dart
@@ -7,7 +7,7 @@ void main() {
   testWidgets('Welcome page renders brand and CTAs', (WidgetTester tester) async {
     await tester.pumpWidget(const MaterialApp(home: WelcomePage()));
 
-    expect(find.text('Barberia Cuts'), findsOneWidget);
+    expect(find.text('Baberia Cuts'), findsOneWidget);
     expect(find.text('Continue as guest'), findsOneWidget);
     expect(find.text('Sign in'), findsOneWidget);
   });
